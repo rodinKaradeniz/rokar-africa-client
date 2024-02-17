5be81a2ec99a7f299cd07310740cfee3a2a8eb70
rodinKaradeniz <mrodin.karadeniz@mail.utoronto.ca>
2024-02-17 16:04:32 -0500

    updated faq

--- src/helpers/Questions.jsx
+++ src/helpers/Questions.jsx
@@ -1,14 +1,15 @@
 export const Questions = [
   {
     question: "When was Rokar Africa founded?",
-    answer: "Rodin",
+    answer: "Rokar Africa was founded in 2019.",
   },
   {
     question: "Where is Rokar Africa Located?",
-    answer: "Tanzania",
+    answer: "Our office is located in Masaki, Dar Es Salaam.",
   },
   {
     question: "How can I make an order or arrange a delivery?",
-    answer: "Tanzania",
+    answer:
+      'Our phone numbers are always reachable within business hours. You can also submit a form requesting an inquiry, from our "Contact Us" page.',
   },
 ];
